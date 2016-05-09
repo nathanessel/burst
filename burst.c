229fe75471a1f4917ea462da2145024e80236174
Nathan Essel <nve1@unix.cs.uakron.edu>
2016-05-09 18:46:28 -0400

    Created burst.c file.

---
+++ burst.c
@@ -0,0 +1,14 @@
+/*
+--------------------------
+Name:	Nathan Essel
+Email:	nve1@zips.uakron.edu
+Program:	burst.c
+Description:	Split a file into separate files based on the number of lines specified by the user.
+--------------------------
+*/
+
+int main(int argc, char* argv[])
+{
+
+
+}
