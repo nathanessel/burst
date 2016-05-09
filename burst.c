30cedacc3c324aabaa9b4e7de7e9973970218bd9
Nathan Essel <nve1@unix.cs.uakron.edu>
2016-05-09 19:26:30 -0400

    Modified burst.c to count the number of lines in a file. Need to functionality to write to output until line segment threshold is reached.

--- burst.c
+++ burst.c
@@ -7,8 +7,29 @@ Description:	Split a file into separate files based on the number of lines speci
 --------------------------
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 int main(int argc, char* argv[])
 {
+  char buffer[16384];
+  int bytes_read;
+  int line_count = 0;
 
+  int fd = open(argv[1], O_RDONLY);
 
+  while (bytes_read = read(fd, buffer, sizeof(buffer)))
+  {
+    int i;
+    for (i=0; i<bytes_read; i++)
+      if (buffer[i] == '\n')
+	++line_count;
+  }
+
+  printf("Number of lines %d\n",line_count);
+  
 }
